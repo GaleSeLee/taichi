4cd76b92b541a10403de8f360ee1a2cccf8f548b
Zeyuli <li_zeyu@pku.edu.cn>
2022-05-11 12:14:18 +0800

    add match_all warp intrinsic by ptx

--- taichi/runtime/llvm/runtime.cpp
+++ taichi/runtime/llvm/runtime.cpp
@@ -1080,15 +1080,27 @@ i32 cuda_match_any_sync_i32(i32 mask, i32 value) {
   return 0;
 }
 
+u32 cuda_match_all_sync_i32(u32 mask, i32 value) {
+#if ARCH_cuda
+    u32 ret;
+    asm volatile("match.all.sync.b32  %0, %1, %2;"
+                 : "=r"(ret)
+                 : "r"(value), "r"(mask));
+    return ret;
+#else
+    return 0;
+#endif
+}
+
 i32 cuda_match_any_sync_i64(i32 mask, i64 value) {
 #if ARCH_cuda
-  u32 ret;
-  asm volatile("match.any.sync.b64  %0, %1, %2;"
-               : "=r"(ret)
-               : "l"(value), "r"(mask));
-  return ret;
+    u32 ret;
+    asm volatile("match.any.sync.b64  %0, %1, %2;"
+                 : "=r"(ret)
+                 : "l"(value), "r"(mask));
+    return ret;
 #else
-  return 0;
+    return 0;
 #endif
 }
 
