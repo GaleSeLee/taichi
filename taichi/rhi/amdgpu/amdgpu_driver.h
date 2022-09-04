44fcd083e2f94c487494c4bd25b07f4130d4945d
Zeyuli <zeyuli@ZeyulideMacBook-Pro.local>
2022-09-04 10:49:03 +0800

    Add taichi/rhi/amdgpu/amdgpu_driver

---
+++ taichi/rhi/amdgpu/amdgpu_driver.h
@@ -0,0 +1,128 @@
+#pragma once
+
+#include <mutex>
+
+#include "taichi/system/dynamic_loader.h"
+#include "taichi/rhi/AMDGPU/amdgpu_types.h"
+
+TLANG_NAMESPACE_BEGIN
+
+// Driver constants from cuda.h
+
+constexpr uint32 HIP_EVENT_DEFAULT = 0x0;
+constexpr uint32 HIP_STREAM_DEFAULT = 0x0;
+constexpr uint32 HIP_STREAM_NON_BLOCKING = 0x1;
+constexpr uint32 HIP_MEM_ATTACH_GLOBAL = 0x1;
+constexpr uint32 HIP_MEM_ADVISE_SET_PREFERRED_LOCATION = 3;
+constexpr uint32 HIP_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_X = 26;
+constexpr uint32 HIP_DEVICE_ATTRIBUTE_MAX_BLOCKS_PER_MULTIPROCESSOR = 25;
+constexpr uint32 HIP_DEVICE_ATTRIBUTE_MULTIPROCESSOR_COUNT = 63;
+// The Capability Major and minor don't represent the version of ROCm
+// Currently, I don't know the meaning of these
+constexpr uint32 HIP_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR = 23;
+constexpr uint32 HIP_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR = 61;
+constexpr uint32 HIP_ERROR_ASSERT = 710;
+constexpr uint32 HIP_JIT_MAX_REGISTERS = 0;
+constexpr uint32 HIP_POINTER_ATTRIBUTE_MEMORY_TYPE = 2;
+constexpr uint32 HIP_DEVICE_ATTRIBUTE_UNIFIED_ADDRESSING = 41;
+constexpr uint32 HIP_SUCCESS = 0;
+constexpr uint32 HIP_MEMORYTYPE_DEVICE = 2;
+
+std::string get_amdgpu_error_message(uint32 err);
+
+template <typename... Args>
+class AMDGPUFunction {
+ public:
+  AMDGPUFunction() {
+    function_ = nullptr;
+  }
+
+  void set(void *func_ptr) {
+    function_ = (func_type *)func_ptr;
+  }
+
+  uint32 call(Args... args) {
+    TI_ASSERT(function_ != nullptr);
+    TI_ASSERT(driver_lock_ != nullptr);
+    std::lock_guard<std::mutex> _(*driver_lock_);
+    return (uint32)function_(args...);
+  }
+
+  void set_names(const std::string &name, const std::string &symbol_name) {
+    name_ = name;
+    symbol_name_ = symbol_name;
+  }
+
+  void set_lock(std::mutex *lock) {
+    driver_lock_ = lock;
+  }
+
+  std::string get_error_message(uint32 err) {
+    return get_cuda_error_message(err) +
+           fmt::format(" while calling {} ({})", name_, symbol_name_);
+  }
+
+  uint32 call_with_warning(Args... args) {
+    auto err = call(args...);
+    TI_WARN_IF(err, "{}", get_error_message(err));
+    return err;
+  }
+
+  // Note: CUDA driver API passes everything as value
+  void operator()(Args... args) {
+    auto err = call(args...);
+    TI_ERROR_IF(err, get_error_message(err));
+  }
+
+ private:
+  using func_type = uint32_t(Args...);
+
+  func_type *function_{nullptr};
+  std::string name_, symbol_name_;
+  std::mutex *driver_lock_{nullptr};
+};
+
+class AMDGPUDriverBase {
+ public:
+  ~AMDGPUDriverBase() = default;
+
+ protected:
+  std::unique_ptr<DynamicLoader> loader_;
+  AMDGPUDriverBase();
+
+  bool load_lib(std::string lib_linux);
+
+  bool disabled_by_env_{false};
+};
+
+class AMDGPUDriver : protected AMDGPUDriverBase {
+ public:
+ // Actually, functions are runtime api
+ // This is not the same as CUDA
+ // The name "amdgpu_driver_functions.inc.h" is to unify with CUDA
+#define PER_AMDGPU_FUNCTION(name, symbol_name, ...) \
+  AMDGPUFunction<__VA_ARGS__> name;
+#include "taichi/rhi/amdgpu/amdgpu_driver_functions.inc.h" 
+#undef PER_AMDGPU_FUNCTION
+
+  char (*get_error_name)(uint32);
+
+  char (*get_error_string)(uint32);
+
+  void (*driver_get_version)(int *);
+
+  bool detected();
+
+  static CUDADriver &get_instance();
+
+  static CUDADriver &get_instance_without_context();
+
+ private:
+  CUDADriver();
+
+  std::mutex lock_;
+
+  // bool rocm_version_valid_{false};
+};
+
+TLANG_NAMESPACE_END
---
+++ taichi/rhi/amdgpu/amdgpu_driver_functions.inc.h
@@ -0,0 +1,51 @@
+// Init
+PER_CUDA_FUNCTION(init, hipInit, unsigned int);
+
+// Device management
+PER_CUDA_FUNCTION(device_get_count, hipGetDeviceCount, int *);
+PER_CUDA_FUNCTION(device_get_attribute, hipDeviceGetAttribute, int *, uint32, int); 
+PER_CUDA_FUNCTION(device_get_name, hipDeviceGetName, char *, int, void *);
+PER_CUDA_FUNCTION(device_get, hipDeviceGet, void *, void *);
+
+// Context management
+PER_CUDA_FUNCTION(context_create, hipCtxCreate, void*, int, void *);
+PER_CUDA_FUNCTION(context_set_current, hipCtxSetCurrent, void *);
+PER_CUDA_FUNCTION(context_get_current, hipCtxGetCurrent, void **);
+
+// Stream management
+PER_CUDA_FUNCTION(stream_create, hipStreamCreate, void **, uint32);
+
+// Memory management
+PER_CUDA_FUNCTION(memcpy_host_to_device, hipMemcpyHtoD, void *, void *, std::size_t);
+PER_CUDA_FUNCTION(memcpy_device_to_host, hipMemcpyDtoH, void *, void *, std::size_t);
+PER_CUDA_FUNCTION(memcpy_device_to_device, hipMemcpyDtoD, void *, void *, std::size_t);
+PER_CUDA_FUNCTION(memcpy, hipMemcpy, void *, void *, std::size_t, unsigned int);
+PER_CUDA_FUNCTION(memcpy_async, hipMemcpyAsync, void *, void *, std::size_t, unsigned int, void *);
+PER_CUDA_FUNCTION(memcpy_host_to_device_async, hipMemcpyHtoDAsync, void *, void *, std::size_t, void *);
+PER_CUDA_FUNCTION(memcpy_device_to_host_async, hipMemcpyDtoHAsync, void *, void *, std::size_t, void *);
+PER_CUDA_FUNCTION(malloc, hipMalloc, void **, std::size_t);
+PER_CUDA_FUNCTION(malloc_managed, hipMallocManaged, void **, std::size_t, uint32);
+PER_CUDA_FUNCTION(memset, hipMemset, void *, uint8, std::size_t);
+PER_CUDA_FUNCTION(mem_free, hipFree, void *);
+PER_CUDA_FUNCTION(mem_get_info, hipMemGetInfo, std::size_t *, std::size_t *);
+
+// Module and kernels
+PER_CUDA_FUNCTION(module_get_function, hipModuleGetFunction, void **, void *, const char *);
+PER_CUDA_FUNCTION(module_load_data_ex, hipModuleLoadDataEx, void **, const char *,
+                    uint32, uint32 *, void **);
+PER_CUDA_FUNCTION(module_load_data, hipModuleLoadData, void **, const void *);
+PER_CUDA_FUNCTION(launch_kernel, hipModuleLaunchKernel, void *, uint32, uint32, uint32,
+                    uint32, uint32, uint32, uint32, void *, void **, void **);
+PER_CUDA_FUNCTION(kernel_get_attribute, hipFuncGetAttribute, int *, uint32, void *);
+PER_CUDA_FUNCTION(kernel_get_occupancy, hipOccupancyMaxActiveBlocksPerMultiprocessor,
+                    int *, void *, int, size_t);
+
+// Stream management
+PER_CUDA_FUNCTION(stream_synchronize, hipStreamSynchronize, void *);
+
+// Event management
+PER_CUDA_FUNCTION(event_create, hipEventCreateWithFlags, void **, uint32);
+PER_CUDA_FUNCTION(event_destroy, hipEventDestroy, void *);
+PER_CUDA_FUNCTION(event_record, hipEventRecord, void *, void *);
+PER_CUDA_FUNCTION(event_synchronize, hipEventSynchronize, void *);
+PER_CUDA_FUNCTION(event_elapsed_time, hipEventElapsedTime, float *, void *, void *);
